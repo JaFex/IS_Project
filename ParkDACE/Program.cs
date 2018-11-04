7986acb3e4fe8533a46ee1af78813dac72024e65
João Félix <jafelix.22@gmail.com>
2018-11-04 14:35:03 +0000

    Project Base

---
+++ ParkDACE/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkDACE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParkingSensorNodeDll.ParkingSensorNodeDll dll = new ParkingSensorNodeDll.ParkingSensorNodeDll();
+
+            dll.Initialize(ComputeResponse, 10000);
+        }
+
+        public static void ComputeResponse(string str)
+        {
+            string[] strs = str.Split(';');
+
+            string[] ids = { "ParkID", "SpotID", "Timestamp", "ParkingSpotStatus", "BatteryStatus" };
+
+            Console.WriteLine("###################################################");
+
+            for(int i = 0; i < strs.Length; i++)
+            {
+                Console.WriteLine(ids[i] + ": " + strs[i]);
+            }
+
+            Console.WriteLine("###################################################\n");
+
+        }
+    }
+}
